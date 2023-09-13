94f2c4696d4a5ba975167a54a6d837432da4668b
qp2194 <qp2194@gmail.com>
2023-09-12 20:22:48 -0500

    copied structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
-// import About from "@/components/about";
-// import Contact from "@/components/contact";
-// import Experience from "@/components/experience";
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Experience from "@/components/experience";
 import Intro from "@/components/intro";
-// import Projects from "@/components/projects";
-// import SectionDivider from "@/components/section-divider";
-// import Skills from "@/components/skills";
+import Projects from "@/components/projects";
+import SectionDivider from "@/components/section-divider";
+import Skills from "@/components/skills";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
-      {/*<SectionDivider />
+      <SectionDivider />
       <About />
+      <SectionDivider />
       <Projects />
+      <SectionDivider />
       <Skills />
+      <SectionDivider />
       <Experience />
-      <Contact /> */}
+      <SectionDivider />
+      <Contact />
     </main>
   );
 }
