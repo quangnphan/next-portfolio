66eccb41b82b45a8b6dad50c959172b1fbb1f0d5
Quang Phan <qp2194@gmail.com>
2023-09-14 16:33:40 -0500

    deleted about page

--- lib/data.ts
+++ lib/data.ts
@@ -13,10 +13,6 @@ export const links = [
     name: "Home",
     hash: "#home",
   },
-  // {
-  //   name: "About",
-  //   hash: "#about",
-  // },
   {
     name: "Projects",
     hash: "#projects",
