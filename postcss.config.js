4d402a19ecc85ce5ab7ca8bdc44cc2c71b1edbbc
Quang Phan <qp2194@gmail.com>
2023-09-23 16:21:05 -0500

    fix config

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {'tailwindcss': any},
-    autoprefixer: {'autoprefixer': any},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
